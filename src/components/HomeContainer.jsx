05791b6965229b3ae1771b747ce372d367d6c47d
leangseu <leangseu_kim@student.uml.edu>
2017-12-30 01:47:33 -0500

    add image to the homepage

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import bridgeSVG from '../images/bridge.svg'
 
 import './styles/HomeContainer.scss'
 
 class HomeContainer extends Component {
   render() {
     return (
-      <div>
-        Home Component
+      <div className='container'>
+          <div className='signupImageContainer'>
+            <span className='campContainer'>
+              <img src={bridgeSVG} alt=''/>
+            </span>
+          </div>
       </div>
     )
   }
